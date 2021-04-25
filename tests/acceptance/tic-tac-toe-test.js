dba34c2ee28a60512b46301132804cab28d8d498
Robbert van Elk <robbert_vanelk@hotmail.com>
2021-04-25 19:07:18 +0200

    Draw board on the index page

---
+++ tests/acceptance/tic-tac-toe-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | tic tac toe', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h1').hasText('Welcome to Tic-Tac-Toe');
+  });
+});
