72660fc36a2fb6d68ffb127568ac3d4ec0a886d4
Robbert van Elk <robbert_vanelk@hotmail.com>
2021-04-25 20:41:03 +0200

    Make it possible to win the game and send feedback

--- app/components/game/index.js
+++ app/components/game/index.js
@@ -1,10 +1,27 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { winningCombinations } from '../../utils/constants';
 
 export default class GameFieldComponent extends Component {
   @tracked gameState = ["", "", "", "", "", "", "", "", ""];
   @tracked currentPlayer = "X";
+  @tracked playing = true;
+  @tracked victoryText = "";
+
+  @action clickCell(index) {
+    if(this.gameState[index] === "" && this.playing) {
+      this.gameState[index] = this.currentPlayer;
+      this.gameState = this.gameState;
+      if(this.checkWinner()){
+        this.playing = false;
+        this.victoryText = "Congratulations " + this.currentPlayer;
+      } else {
+        this.switchPlayer();
+      }
+
+    }
+  }
 
   @action switchPlayer() {
     if (this.currentPlayer === "O") {
@@ -14,11 +31,19 @@ export default class GameFieldComponent extends Component {
     }
   }
 
-  @action clickCell(index) {
-    if(this.gameState[index] === "") {
-      this.gameState[index] = this.currentPlayer;
-      this.gameState = this.gameState;
-      this.switchPlayer();
-    }
+  @action checkWinner() {
+    let state = this.gameState
+    let current = this.currentPlayer
+    let match = false;
+    winningCombinations.forEach(function(element) {
+      let arr = element.every(function(num) {
+        return state[num] === current
+      })
+      if(arr) {
+        match = true
+        return;
+      }
+    })
+    return match;
   }
 }
--- tests/acceptance/tic-tac-toe-test.js
+++ tests/acceptance/tic-tac-toe-test.js
@@ -10,11 +10,38 @@ module('Acceptance | tic tac toe', function(hooks) {
 
     assert.equal(currentURL(), '/');
     assert.dom('h1').hasText('Welcome to Tic-Tac-Toe');
+
+  });
+
+  test('Click fields', async function(assert) {
+    await visit('/');
+
     await click('.field:nth-child(1)');
     assert.dom('.field:nth-child(1)').hasText('X');
     await click('.field:nth-child(1)');
     assert.dom('.field:nth-child(1)').hasText('X');
     await click('.field:nth-child(2)');
     assert.dom('.field:nth-child(2)').hasText('O');
+
+  });
+
+  test('Let a player win a game', async function(assert) {
+    await visit('/');
+
+    await click('.field:nth-child(1)');
+    assert.dom('.field:nth-child(1)').hasText('X');
+    await click('.field:nth-child(4)');
+    assert.dom('.field:nth-child(4)').hasText('O');
+    await click('.field:nth-child(2)');
+    assert.dom('.field:nth-child(2)').hasText('X');
+    await click('.field:nth-child(5)');
+    assert.dom('.field:nth-child(5)').hasText('O');
+    await click('.field:nth-child(3)');
+    assert.dom('.field:nth-child(3)').hasText('X');
+
+    assert.dom('label').hasText('Congratulations X');
+
+    await click('.field:nth-child(6)');
+    assert.dom('.field:nth-child(6)').hasText('');
   });
 });
